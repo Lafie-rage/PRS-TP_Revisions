5f06ad4dda9859cd4b0dd7e9c3ddabe9c48a510e
Lafie-rage <corentin.destrez62@gmail.com>
2020-09-16 15:14:37 +0200

    Correcting translation function

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ Version ..... : V0.1 du 16/09/2020
 ********************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
@@ -62,7 +63,9 @@ const char MORSE[45][7] = {
 
 const int TAILLE_CARACTERE_MORSE = 7;
 
-char* traductionVersMorse(char* message);
+const int MAX_SENTENCE_LENGTH = 250;
+
+const char* traductionVersMorse(char* message);
 
 char t(int Mod);
 char p(int Mod);
@@ -72,10 +75,10 @@ void traduction(char lettre, int Mod);
 int main()
 {
 	int select = 0,i=0, Mod=0;
-	
+
 	// Mot temporaire pour débug au lieu de retaper à chaque fois du texte
-	char mot[255] = "chanson vraiment bien";
-	
+	char mot[MAX_SENTENCE_LENGTH];
+
 	while (select != -1)
     {
         printf("\tTraduction Morse\n\n");
@@ -83,28 +86,29 @@ int main()
         printf("1 - Modifier le texte a traduire\n");
         printf("2 - Traduction a l'ecran en texte\n");
         printf("3 - Traduction vers du materiel\n");
-        printf("4 - Quitter l'application\n");
+        printf("0 - Quitter l'application\n");
 
         scanf("%d", &select);
-	
+        fgetc(stdin); // Eviter le \n du scanf précédent...
+
 		switch(select)
         {
             case 1:
                 printf("Texte a traduire : \n");
-                scanf("\n%[^\n]s",&mot);
+                fgets(mot, MAX_SENTENCE_LENGTH, stdin);
                 break;
 
             case 2:
                 printf("Vous avez choisi la traduction textuelle\n\n");
                 Mod=0;
-                while(mot[i]!='\0')
+                /*while(mot[i]!='\0')
                 {
                     printf("%c : ", mot[i]);
                     traduction(mot[i], Mod);
                     printf("\n");
                     i++;
-                }
-                //printf("%s", traductionVersMorse(mot));
+                }*/
+                printf("%s\n\n\n", traductionVersMorse(mot));
                 i=0;
                 break;
 
@@ -113,146 +117,37 @@ int main()
                 Mod=1;
                 break;
 
-            case 4:
-                select = -1;
-                break;
-
             default:
+              return 0;
                 break;
 		}
 	}
-	
 	return 0;
 }
 
 
-char* traductionVersMorse(char* message) {
+const char* traductionVersMorse(char* message) {
   int i = 0,
-	  j;
-  char messageTraduit[(strlen(message) -1) * TAILLE_CARACTERE_MORSE + 1];
+	  j = 0;
+  char *messageTraduit = malloc((strlen(message) -1) * TAILLE_CARACTERE_MORSE + 1);
   char caractereMorse[TAILLE_CARACTERE_MORSE];
   int cmpMessageTraduit = 0;
   while(message[i] != '\0') {
-	  printf("%d", i);
-      message[i] = toupper(message[i++]);
-  }
-  printf("MESSAGE : %s\n", message);
-	
-  while(message[i] != '\0') { // Traduction du message en morse
-	printf("%d", cmpMessageTraduit);
-    if(message[i] == ' ') strcat(caractereMorse, " / ");
-    else strcat(caractereMorse, MORSE[message[i]-46]);
+    message[i] = toupper(message[i]);
     i++;
-    while(caractereMorse[i] != '\n' && caractereMorse[i] != '\0') {
-      messageTraduit[cmpMessageTraduit++] = caractereMorse[j];
-    }
-   messageTraduit[cmpMessageTraduit++] = ' ';  
   }
-
-}
-
-void traduction(char lettre, int Mod)
-    {
-        switch(lettre)
-        {
-            case 'a':
-                printf("%c%c", p(Mod), t(Mod));
-                break;
-            case 'b':
-                printf("%c%c%c%c", t(Mod),p(Mod),p(Mod),p(Mod));
-                break;
-            case 'c':
-                printf("%c%c%c%c", t(Mod),p(Mod),t(Mod),p(Mod));
-                break;
-            case 'd':
-                printf("%c%c%c", t(Mod),p(Mod),p(Mod));
-                break;
-            case 'e':
-                printf("%c", p(Mod));
-                break;
-            case 'f':
-                printf("%c%c%c%c", p(Mod),p(Mod),t(Mod),p(Mod));
-                break;
-            case 'g':
-                printf("%c%c%c", t(Mod),t(Mod),p(Mod));
-                break;
-            case 'h':
-                printf("%c%c%c%c", p(Mod),p(Mod),p(Mod),p(Mod));
-                break;
-            case 'i':
-                printf("%c%c", p(Mod),p(Mod));
-                break;
-            case 'j':
-                printf("%c%c%c%c", p(Mod),t(Mod),t(Mod),t(Mod));
-                break;
-            case 'k':
-                printf("%c%c%c", t(Mod),p(Mod),t(Mod));
-                break;
-            case 'l':
-                printf("%c%c%c%c", p(Mod),t(Mod),p(Mod),p(Mod));
-                break;
-            case 'm':
-                printf("%c%c", t(Mod),t(Mod));
-                break;
-            case 'n':
-                printf("%c%c", t(Mod),p(Mod));
-                break;
-            case 'o':
-                printf("%c%c%c", t(Mod),t(Mod),t(Mod));
-                break;
-            case 'p':
-                printf("%c%c%c%c", p(Mod),t(Mod),t(Mod),p(Mod));
-                break;
-            case 'q':
-                printf("%c%c%c%c", t(Mod),t(Mod),p(Mod),t(Mod));
-                break;
-            case 'r':
-                printf("%c%c%c", p(Mod),t(Mod),p(Mod));
-                break;
-            case 's':
-                printf("%c%c%c", p(Mod),p(Mod),p(Mod));
-                break;
-            case 't':
-                printf("%c", t(Mod));
-                break;
-            case 'u':
-                printf("%c%c%c", p(Mod),p(Mod),t(Mod));
-                break;
-            case 'v':
-                printf("%c%c%c%c", p(Mod),p(Mod),p(Mod),t(Mod));
-                break;
-            case 'w':
-                printf("%c%c%c", p(Mod),t(Mod),t(Mod));
-                break;
-            case 'x':
-                printf("%c%c%c%c", t(Mod),p(Mod),p(Mod),t(Mod));
-                break;
-            case 'y':
-                printf("%c%c%c%c", t(Mod),p(Mod),t(Mod),t(Mod));
-                break;
-            case 'z':
-                printf("%c%c%c%c", t(Mod),t(Mod),p(Mod),p(Mod));
-                break;
-            default:
-                break;
-        }
+  i = 0;
+  while(message[i] != '\n') { // Traduction du message en morse
+    if(message[i] == ' ') strcpy(caractereMorse, " / ");
+    else strcpy(caractereMorse, MORSE[message[i]-46]);
+    while(caractereMorse[j] != '\0') {
+      messageTraduit[cmpMessageTraduit] = caractereMorse[j];
+      cmpMessageTraduit++;
+      j++;
     }
-    
-    // affiche un tiret
-char t(int Mod)
-{
-    if(Mod==0)
-        return '-';
-    else
-        return '-';
+    j = 0;
+    i++;
+   messageTraduit[cmpMessageTraduit++] = ' ';
+  }
+  return messageTraduit;
 }
-
-// affiche un point
-char p(int Mod)
-{
-    if(Mod==0)
-        return 'o';
-    else
-        return 'o';
-}
-
